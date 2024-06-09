be40cc67fd3e8beaa17f1dfc1847486952513020
Justin Calleja <calleja.justin@gmail.com>
2024-06-09 21:05:45 +0200

    use built-in scripts

--- scripts/node/has_btn_with_sf_disabled/toggle_disabled.gd
+++
@@ -1,9 +0,0 @@
-@tool
-extends HasBtnWithSfDisabled
-
-
-func toggle_target_btn_disabled() -> void:
-	if btn_with_sf_disabled.disabled:
-		btn_with_sf_disabled.enable()
-	else:
-		btn_with_sf_disabled.disable()
