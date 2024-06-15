836baad0c4fea0d40b4f323d57a28e18a55de971
Justin Calleja <calleja.justin@gmail.com>
2024-06-15 07:27:58 +0200

    new approach (tree traversal)

--- scenes/crazy_btns/context.gd
+++ scenes/crazy_btns/context.gd
@@ -1,5 +1,5 @@
 @tool
-class_name SrcAndTargetBtns
+class_name CrazyBtnsContext
 extends Node
 
 @export var src_btn: Button:
@@ -9,6 +9,14 @@ extends Node
 		src_btn = node
 		update_configuration_warnings()
 
+@export var src_label: Label:
+	get:
+		return src_label
+	set(node):
+		src_label = node
+		update_configuration_warnings()
+
+var src_btn_press_count: int = 0
 
 @export var target_btn: Button:
 	get:
@@ -17,6 +25,14 @@ extends Node
 		target_btn = node
 		update_configuration_warnings()
 
+@export var target_label: Label:
+	get:
+		return target_label
+	set(node):
+		target_label = node
+		update_configuration_warnings()
+
+var target_btn_press_count = 0
 
 func _ready():
 	if Engine.is_editor_hint():	return
@@ -25,7 +41,10 @@ func _ready():
 
 func _post_ready_assertions() -> void:
 	assert(is_instance_valid(src_btn), "src_btn is not valid")
+	assert(is_instance_valid(src_label), "src_label is not valid")
+
 	assert(is_instance_valid(target_btn), "target_btn is not valid")
+	assert(is_instance_valid(target_label), "target_label is not valid")
 
 
 func _get_configuration_warnings():
@@ -33,7 +52,12 @@ func _get_configuration_warnings():
 
 	if not is_instance_valid(src_btn):
 		warnings.append("src_btn is not valid")
+	if not is_instance_valid(src_label):
+		warnings.append("src_label is not valid")
+
 	if not is_instance_valid(target_btn):
 		warnings.append("target_btn is not valid")
+	if not is_instance_valid(target_label):
+		warnings.append("target_label is not valid")
 
 	return warnings
--- scripts/Node/TNode.gd
+++ scripts/Node/TNode.gd
@@ -1,16 +1,16 @@
 class_name TNode
 extends Node
-## A Node that be visited and its children to run logic.
+## A "Traversable" Node.
 ##
 ## TODO: add description and fix brief description above
 ## (see: https://docs.godotengine.org/en/stable/tutorials/scripting/gdscript/gdscript_documentation_comments.html)
 ##
 
-func traverseBF(fn_name: String, context: Variant) -> void:
+func traverseDF(fn_name: String, context: Variant) -> void:
 	if has_method(fn_name):
 		print("Visiting ", self.name)
 		call(fn_name, context)
 
 	for child in get_children():
-		child.traverseBF(fn_name, context)
+		child.traverseDF(fn_name, context)
 
